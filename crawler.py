dc5e1d680ae456a8e2ae615b4fc3292b295af76a
matheAlves <matheus.souza.alves@gmail.com>
2022-11-07 22:12:59 -0300

    feat: selenium semi implemented

--- crawler.py
+++ crawler.py
@@ -1,6 +1,12 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
 import requests
-from parsel import Selector
+import time
 from bs4 import BeautifulSoup
+
+INITIAL_URL = "https://www.wizardingworld.com/writing-by-jk-rowling"
+browser = webdriver.Chrome()
+
 
 def fetch_content(url):
     try:
@@ -11,12 +17,44 @@ def fetch_content(url):
     return response
 
 
-# it is necessary to find a way to scroll the page for all the url to be scraped
-def extract_urls(content):
-    selector = Selector(content.text)
-    urls = selector.css("div.Hub_root__qduRu a::attr(href)").getall()
-    urls.pop(0)
+def extract_urls():
+    urls = [
+        url.get_attribute("href")
+        for url in browser.find_element(By.CLASS_NAME, "Hub_root__qduRu").find_elements(
+            By.CSS_SELECTOR, "a"
+        )
+    ]
     return urls
+
+
+def scroll():
+    browser.get("https://www.wizardingworld.com/writing-by-jk-rowling")
+
+
+    print(len(urls))
+    browser.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+    time.sleep(2)
+    urls = extract_urls()
+    print(len(urls))
+    browser.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+    time.sleep(2)
+    urls = extract_urls()
+    print(len(urls))
+
+    # urls = selector.css("div.Hub_root__qduRu a::attr(href)").getall()
+    # urls.pop(0)
+    # return urls
+
+
+# current_urls = extract_urls(fetch_content(INITIAL_URL))
+
+# while True:
+#     def scroll_page():
+
+#         browser.get("https://www.wizardingworld.com/writing-by-jk-rowling")
+#         browser.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+
+# it is necessary to find a way to scroll the page for all the url to be scraped
 
 
 def extract_content(urls):
@@ -24,7 +62,7 @@ def extract_content(urls):
     BASE_URL = "https://www.wizardingworld.com"
     try:
         for url in urls:
-            first_word = ''
+            first_word = ""
             content = fetch_content(BASE_URL + url)
             soup = BeautifulSoup(content.content, "html.parser")
             title = soup.find(class_="ArticleHero_title__cOam6").text
@@ -32,18 +70,10 @@ def extract_content(urls):
             extract = [span.extract().text for span in content.find_all("span")]
             if extract:
                 first_word = extract[1]
-                text = first_word + ' ' + content.get_text(strip=True)
+                text = first_word + " " + content.get_text(strip=True)
             else:
                 text = content.get_text(strip=True)
             result.append({"title": title, "text": text})
     except Exception as err:
         print(err.args)
     return result
-
-def crawl():
-    INITIAL_URL = "https://www.wizardingworld.com/writing-by-jk-rowling"
-    content = fetch_content(INITIAL_URL)
-    urls = extract_urls(content)
-    return extract_content(urls)
-
-    
